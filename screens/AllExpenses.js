5abebce84842f7dfa33c04fdb3ac9c12d35cdb11
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-05-03 14:01:22 +0300

    finishing touches

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -6,7 +6,7 @@ import { ExpensesContext } from "../store/expenses-contex"
 const AllExpenses = () => {
   const expensesCtx=useContext(ExpensesContext)
   return (
-    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total"/>
+    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found."/>
   )
 }
 
